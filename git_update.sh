68642fd83b41bc2af1c337d8f11d289de22f20c2
dondvdlaan <dondvdlaan@gmail.com>
2023-08-30 13:05:33 +0200

    Small improvements persistence

--- git_update.sh
+++ git_update.sh
@@ -9,7 +9,7 @@ chmod u+x git_update.sh
 #cd idea-IU-231.8109.175/bin
 
 git add .
-git commit -m "Adding Logger and Comments"
+git commit -m "Small improvements persistence"
 git push Java-Spring-Microservices-Kafka
 
 
